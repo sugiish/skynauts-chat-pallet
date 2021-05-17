1b7d9ec882735b26a092a8eedcdde70029706b9f
sugiish <sugiish.me@gmail.com>
2021-05-17 19:32:07 +0900

    Update chat pallets for a new bot

--- chat_pallet.rb
+++ chat_pallet.rb
@@ -6,10 +6,10 @@ Action = Struct.new(:label, :requirements, :difficulty)
 ACTIONS = [
   Action.new('砲撃', %w[技術 感覚], 7),
   Action.new('修理', %w[技術 教養], 7),
-  Action.new('操舵', %w[感覚 教養], '?'),
+  Action.new('操舵', %w[感覚 教養], 7),
   Action.new('回避', %w[感覚 教養], 7),
   Action.new('侵入', %w[身体 技術], 7),
-  Action.new('白兵', %w[身体 技術], '?'),
+  Action.new('白兵', %w[身体 技術], 7),
   Action.new('偵察', %w[身体 感覚], 7), # 電探のみ「技術・教養：8」
   Action.new('大揺れ', %w[身体 感覚], 7),
   Action.new('消火', %w[身体 教養], 7),
@@ -27,24 +27,24 @@ def print_chat_pallets(strong_points, weak_point, options)
   end
 end
 
-def convert_chat_pallet(label, strong_count, weak, options, difficulty='?')
+def convert_chat_pallet(label, strong_count, weak, options, difficulty=7)
   case
   when strong_count > 0 && weak
     if options[:craftsman]
-      "3d6kh2>=#{difficulty} 【#{label}+(-)】"
+      "3sn#{difficulty}#1 【#{label}+(-)】"
     else
-      "3d6kh2>=#{difficulty} 【#{label}±】"
+      "3sn#{difficulty}#2 【#{label}±】"
     end
   when strong_count > 0
     if options[:specialist] && strong_count == 2
-      "4d6kh2>=#{difficulty} 【#{label}*】"
+      "4sn#{difficulty}#1 【#{label}*】"
     else
-      "3d6kh2>=#{difficulty} 【#{label}+】"
+      "3sn#{difficulty}#1 【#{label}+】"
     end
   when weak
-    "2d6>=#{difficulty} 【#{label}-】"
+    "2sn#{difficulty}#2 【#{label}-】"
   else
-    "2d6>=#{difficulty} 【#{label}】"
+    "2sn#{difficulty}#1 【#{label}】"
   end
 end
 
